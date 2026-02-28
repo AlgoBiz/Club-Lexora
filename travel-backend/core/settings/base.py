7d9da1962e4cc2a6df53a048e379ac3982a4dde6
Afsal-kallingal <moafsalkallingal1088@gmail.com>
2026-02-28 12:49:58 +0530

    cors

--- travel-backend/core/settings/base.py
+++ travel-backend/core/settings/base.py
@@ -92,8 +92,13 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+]
+# OR if you want open for now:
+# CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_HEADERS = [
     'accept',
     'accept-encoding',
