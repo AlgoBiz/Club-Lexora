b8f9bdca7d29d16acae02b04edbbc53028e57b15
Afsal-kallingal <moafsalkallingal1088@gmail.com>
2026-02-28 13:15:51 +0530

    cors

--- travel-backend/core/settings/base.py
+++ travel-backend/core/settings/base.py
@@ -86,11 +86,11 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8080",
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://localhost:8080",
+# ]
 
 
 STATIC_URL = '/static/'
@@ -103,13 +103,17 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-]
-# OR if you want open for now:
-# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8080",
+#     "http://127.0.0.1:8080",
+#     "https://club-luxoura-dev.netlify.app",
+# ]
 
-CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = False
+
+# CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
     'accept',
     'accept-encoding',
@@ -122,6 +126,8 @@ CORS_ALLOW_HEADERS = [
     'x-requested-with',
     'Role'
 ]
+
+
 CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
 
 TEMPLATES = [
